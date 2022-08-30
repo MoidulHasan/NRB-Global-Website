22d23696e3d844517ed08116a65656038a84442c
hasan75 <hasanjab14@gmail.com>
2022-08-31 01:29:38 +0600

    added blog component in app.js for #3

---
+++ src/pages/Blogs/BlogPage/BlogPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h2>This is the blog pager</h2>
+    </div>
+  );
+};
+
+export default BlogPage;
+<h2>This is the blog pager</h2>;
