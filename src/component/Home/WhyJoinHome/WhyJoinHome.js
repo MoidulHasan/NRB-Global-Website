45189405ce126b60e199f0eb299b7c1eac81c5bd
hasan75 <hasanjab14@gmail.com>
2022-09-02 11:08:04 +0600

    this commit fullfill the purpose of #3

--- src/component/Home/WhyJoinHome/WhyJoinHome.js
+++ src/component/Home/WhyJoinHome/WhyJoinHome.js
@@ -6,6 +6,7 @@ import { Button } from 'primereact/button';
 const WhyJoinHome = () => {
   return (
     <div className='p-container mb-4'>
+      {/* why join nrb home  */}
       <div className='sectionWhyContainer  py-5 px-4'>
         <h1 className='text-4xl md:text-5xl mt-3 mb-3 md:mb-6 textWhy text-center'>
           Why Join NRB Global?{' '}
