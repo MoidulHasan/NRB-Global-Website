aeac1e6e8e15f727659f9fb936ce5a25f323e885
hasan75 <hasanjab14@gmail.com>
2022-09-02 19:00:57 +0600

    all member list added and fixes #8

--- src/component/Membership/Members/MemberList/MemberListHeader/MemberListHeader.js
+++ src/component/Membership/Members/MemberList/MemberListHeader/MemberListHeader.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 import './MemberListHeader.css';
 
 const MemberListHeader = () => {
   return (
     <div className='p-container mb-4 mt-3 md:mt-5'>
       <div className='memberContainer py-5 px-4 flex flex-column align-items-center justify-content-center'>
-        <div className='textDiv flex flex-column align-items-center justify-content-center '>
-          <h2 className='text-2xl md:text-5xl mb-2'>Our Honourable Members</h2>
-          <span className='text-dark text-xl font-bold'>A Family We Are</span>
-        </div>
+        <Fade top duration={1500}>
+          <div className='textDiv flex flex-column align-items-center justify-content-center '>
+            <h2 className='text-2xl md:text-5xl mb-2'>
+              Our Honourable Members
+            </h2>
+            <span className='text-dark text-xl font-bold'>A Family We Are</span>
+          </div>
+        </Fade>
       </div>
     </div>
   );
--- src/pages/Membership/Members/MemberList/MemberList.js
+++ src/pages/Membership/Members/MemberList/MemberList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MemberListHeader from '../../../../component/Membership/Members/MemberList/MemberListHeader/MemberListHeader';
 import MemberListToSHow from '../../../../component/Membership/Members/MemberList/MemberListToSHow/MemberListToSHow';
+import './Meberlist.css';
+import { Button } from 'primereact/button';
 
 const MemberList = () => {
   return (
@@ -10,6 +12,24 @@ const MemberList = () => {
       </section>
       <section>
         <MemberListToSHow />
+      </section>
+      <section className='p-container my-4'>
+        <div className='beAnrbMember py-5 px-4 flex flex-column align-items-center justify-content-center'>
+          <div>
+            <h2 className='my-2 excitedJoinText'>
+              We are very excited to <span className='seeYou'>see you</span> in
+              the member list
+            </h2>
+          </div>
+          <div className='flex justify-content-center mt-3'>
+            <Button
+              label='Join NRB Global'
+              className='p-button-rounded p-button-raised p-button-help p-button-outlined'
+              icon='pi pi-angle-right'
+              iconPos='right'
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
