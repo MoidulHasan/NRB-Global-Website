bb9617f225688fce05ef4075124637196584bfb6
najim473 <najim7304@gmail.com>
2022-09-03 20:21:08 +0600

    modified gallary page

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -237,7 +237,7 @@ const Header = () => {
             </Link>
           </li>
           <li className='nav-item'>
-            <Link to='/blogs' className='nav-links' onClick={closeMobileMenu}>
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
               Contact
             </Link>
           </li>
