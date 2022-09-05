d16377bb3f0c62becd48de3ec198e0ba11232717
najim473 <najim7304@gmail.com>
2022-09-05 17:56:22 +0600

    added gmap and contact form

--- src/component/Contact/ContactForm.js
+++ src/component/Contact/ContactForm.js
@@ -7,17 +7,18 @@ function ContactForm() {
     return (
         <div className="contact-form-conatiner">
             {/* <form onSubmit={formik.handleSubmit} className="p-fluid"> */}
-            <div className="contact-form-title">
-                <h5>Leave us a message</h5>
-            </div>
+
             <form className="contact-form">
+                <div className="contact-form-title">
+                    <h5>Leave us a message</h5>
+                </div>
                 <div className='contact-form-grid grid '>
                     <InputText type="text" className="mb-2 col-12" placeholder="Name" />
                     <InputText type="email" className="mb-2 col-12" placeholder="Email" />
                     <InputText type="number" className="mb-2 col-12" placeholder="Phone" />
                     <InputText type="text" className=" mb-2 col-12" placeholder="Subject" />
                     {/* <InputTextarea value={value1} onChange={(e) => setValue1(e.target.value)} rows={5} cols={30} /> */}
-                    <InputTextarea className='col-12' placeholder="Comment" rows={5} cols={30} />
+                    <InputTextarea className='col' placeholder="Comment" rows={5} cols={30} />
                 </div>
                 <Button type="submit" label="Submit" className="contact-form-submit-btn mt-2" />
             </form>
--- src/component/Contact/ContactInfo.js
+++ src/component/Contact/ContactInfo.js
@@ -8,7 +8,7 @@ function ContactInfo() {
         <div className="contactinfo-container ">
             <div className="contactinfo-grid-container grid">
                 <div className="contactinfo-bn col-12 grid ">
-                    <div className="contactinfo-bn-txt col-6">
+                    <div className="contactinfo-bn-txt col lg:col-6 md:col-12">
                         <div className="contactinfo-bn-details">
                             <div className="contactinfo-bn-title">
                                 <h2>Bangladesh Office</h2>
@@ -45,13 +45,13 @@ function ContactInfo() {
                             </div>
                         </div>
                     </div>
-                    <div className="contactinfo-us-txt col-6">
+                    <div className="contactinfo-map-txt col lg:col-6 md:col-12">
                         <GMapbn />
                     </div>
                 </div>
                 <div className="contactinfo-us col-12 grid">
-                    <div className="col-6"><GMapus /></div>
-                    <div className="col-6">
+                    <div className="col lg:col-6 md:col-12 lg:flex-order-1 md:flex-order-2"><GMapus /></div>
+                    <div className="col lg:col-6 md:col-12">
                         <div className="contactinfo-us-txt">
                             <div className="contactinfo-us-details">
                                 <div className="contactinfo-us-title">
