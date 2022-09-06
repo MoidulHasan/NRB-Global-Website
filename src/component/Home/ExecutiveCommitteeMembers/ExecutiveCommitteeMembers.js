1a5203c758550c34fba5bbae28ffb78645ae217f
hasan75 <hasanjab14@gmail.com>
2022-09-07 02:36:39 +0600

    executivemembers in header is added for #19

---
+++ src/component/Home/ExecutiveCommitteeMembers/ExecutiveCommitteeMembers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExecutiveCommitteeMembers.css';
+
+const ExecutiveCommitteeMembers = () => {
+  return (
+    <div className='p-container my-4'>
+      <h2>Executive Members</h2>
+    </div>
+  );
+};
+
+export default ExecutiveCommitteeMembers;
