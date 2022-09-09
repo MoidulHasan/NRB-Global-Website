a22e512a9464d397a2c48139262f9feb4b2381a3
najim473 <najim7304@gmail.com>
2022-09-10 00:00:34 +0600

    created review-page-responsiveness

--- src/component/Contact/ContactInfo.js
+++ src/component/Contact/ContactInfo.js
@@ -45,13 +45,13 @@ function ContactInfo() {
                             </div>
                         </div>
                     </div>
-                    <div className="contactinfo-map-txt col lg:col-6 md:col-12">
+                    <div className="contactinfo-map-txt col lg:col-6 md:col-12 sm:col-12 ">
                         <GMapbn />
                     </div>
                 </div>
                 <div className="contactinfo-us col-12 grid">
                     {/* <div className="col lg:col-6 md:col-12 lg:flex-order-1 md:flex-order-2"><GMapus /></div> */}
-                    <div className="col lg:col-6 md:col-12"><GMapus /></div>
+                    <div className="flex-order-2 col lg:col-6 md:col-12 sm:col-12 "><GMapus /></div>
                     <div className="col lg:col-6 md:col-12 sm:col-12">
                         <div className="contactinfo-us-txt">
                             <div className="contactinfo-us-details">
