f8caba5b1e326699df54d1137e88ac534e82c525
najim473 <najim7304@gmail.com>
2022-09-18 21:37:21 +0600

    young-congress/ycateam.css refectore

--- src/component/YoungCongress/YCAbout/YCATeam.js
+++ src/component/YoungCongress/YCAbout/YCATeam.js
@@ -9,20 +9,20 @@ function YCATeam() {
         <div className='ycateam-container'>
             {/* <Card title="Simple Card" style={{ width: '25rem', marginBottom: '2em' }}> */}
             <h1 className="ycateam-header">Young congress members</h1>
-            <div className="ycm grid">
+            <div className="ycmteam grid">
                 {nrbMembers.slice(0, 8).map((acateam, i) => (
-                    <div className="ycm-profile lg:col-3 md:col-6 py-3">
-                        <div className="ycm-profile-flex-container flex align-items-cetner justify-content-center">
-                            <Card className='ycm-card' style={{ width: '17em' }} key={i}>
+                    <div className="ycmteam-profile lg:col-3 md:col-6 py-3">
+                        <div className="ycmteam-profile-flex-container flex align-items-cetner justify-content-center">
+                            <Card className='ycmteam-card' style={{ width: '17em' }} key={i}>
                                 {/* <p className="cabinetcardquote" style={{ lineHeight: '1.5' }}> {acateam.description}
                         </p> */}
-                                <div className="ycm-flex">
+                                <div className="ycmteam-flex">
                                     <img src={acateam.image} alt="img" />
-                                    <div className="ycm-flex1">
-                                        <p className='ycm-name'>{acateam.name}</p>
-                                        <h5 className='ycm-title'>{acateam.designation}</h5>
-                                        <div className="ycm-flexSocialandFlagicons">
-                                            <div className="ycm-socialicons">
+                                    <div className="ycmteam-flex1">
+                                        <p className='ycmteam-name'>{acateam.name}</p>
+                                        <h5 className='ycmteam-title'>{acateam.designation}</h5>
+                                        <div className="ycmteam-flexSocialandFlagicons">
+                                            <div className="ycmteam-socialicons">
                                                 <a href={acateam.fblink}>
                                                     <i className="pi pi-facebook" style={{ color: '#333' }} />
                                                 </a>
@@ -48,7 +48,7 @@ function YCATeam() {
                 ))}
             </div>
             <div className="ycateam-btn">
-                {/* <Button className='ycm-join-btn' label="Become a Young Congress" /> */}
+                {/* <Button className='ycmteam-join-btn' label="Become a Young Congress" /> */}
                 <Link to='/YCMembers'>
                     <button>See More Members</button>
                 </Link>
