5def52f7ce7f676428005965017b8bb38f179faf
hasan75 <hasanjab14@gmail.com>
2022-09-27 17:04:38 +0600

    a modal added for the anniversary

--- src/App.js
+++ src/App.js
@@ -1,84 +1,98 @@
-import "./App.css";
+import './App.css';
 
 // primeReact imports
-import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
-import "primereact/resources/primereact.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
-import "primeflex/primeflex.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css';
 
 // flag icon
-import "/node_modules/flag-icons/css/flag-icons.min.css";
+import '/node_modules/flag-icons/css/flag-icons.min.css';
 
-import { ScrollTop } from "primereact/scrolltop";
+import { ScrollTop } from 'primereact/scrolltop';
 
-import ContextProvider from "./contexts/ContextProvider";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Header from "./component/Header/Header";
-import Contact from "./pages/Contact/Contact";
-import Blog from "./pages/Blogs/Blog";
-import EventPage from "./pages/Events/EventPage/EventPage";
+import ContextProvider from './contexts/ContextProvider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Header from './component/Header/Header';
+import Contact from './pages/Contact/Contact';
+import Blog from './pages/Blogs/Blog';
+import EventPage from './pages/Events/EventPage/EventPage';
 
-import Footer from "./component/Footer/Footer";
-import MemberList from "./pages/Membership/Members/MemberList/MemberList";
-import MemberServices from "./pages/Membership/Members/MemberServices/MemberServices";
-import GeneralMembers from "./pages/Membership/GeneralMembers/GeneralMembersList/GeneralMembers";
-import GeneralMemberRegistration from "./pages/Membership/GeneralMembers/GeneralMemberRegistration/GeneralMemberRegistration";
-import EMIndividualRegistration from "./pages/Membership/ExecutiveMember/EMIndividualRegistration";
-import EMOrganization from "./pages/Membership/ExecutiveMember/EMOrganization";
-import MembershipProcess from "./pages/Membership/MembershipProcess/MembershipProcess";
-import AllServices from "./pages/Services/AllServices/AllServices";
-import SingleServicePage from "./pages/Services/AllServices/SingleServicePage/SingleServicePage";
-import BlogDetailSingle from "./pages/Blogs/BlogDetails/BlogDetailSingle";
-import EventDetailSingle from "./pages/Events/EventDetails/EventDetailSingle";
-import YCAbout from "./pages/YoungCongress/YCAbout/YCAbout";
-import YCMembers from "./pages/YoungCongress/YCMembers/YCMembers";
-import YCProgram from "./pages/YoungCongress/YCProgram/YCProgram";
-import YCAtivities from "./pages/YoungCongress/YCActivities/YCAtivities";
-import CabinateMembers from "./pages/Commitees/CabinateMembers/CabinateMembers";
-import ExecutiveAbassador from "./pages/Commitees/ExecutiveAmbassador/ExecutiveAbassador";
-import AdvisorCouncil from "./pages/Commitees/AdvisoryCouncil/AdvisorCouncil";
-import PhotoGallary from "./pages/Galllery/PhotoGallery/PhotoGallary";
-import Gallery from "./pages/Galllery/Gallery";
-import VideoGallary from "./pages/Galllery/VideoGallery/VideoGallary";
+import Footer from './component/Footer/Footer';
+import MemberList from './pages/Membership/Members/MemberList/MemberList';
+import MemberServices from './pages/Membership/Members/MemberServices/MemberServices';
+import GeneralMembers from './pages/Membership/GeneralMembers/GeneralMembersList/GeneralMembers';
+import GeneralMemberRegistration from './pages/Membership/GeneralMembers/GeneralMemberRegistration/GeneralMemberRegistration';
+import EMIndividualRegistration from './pages/Membership/ExecutiveMember/EMIndividualRegistration';
+import EMOrganization from './pages/Membership/ExecutiveMember/EMOrganization';
+import MembershipProcess from './pages/Membership/MembershipProcess/MembershipProcess';
+import AllServices from './pages/Services/AllServices/AllServices';
+import SingleServicePage from './pages/Services/AllServices/SingleServicePage/SingleServicePage';
+import BlogDetailSingle from './pages/Blogs/BlogDetails/BlogDetailSingle';
+import EventDetailSingle from './pages/Events/EventDetails/EventDetailSingle';
+import YCAbout from './pages/YoungCongress/YCAbout/YCAbout';
+import YCMembers from './pages/YoungCongress/YCMembers/YCMembers';
+import YCProgram from './pages/YoungCongress/YCProgram/YCProgram';
+import YCAtivities from './pages/YoungCongress/YCActivities/YCAtivities';
+import CabinateMembers from './pages/Commitees/CabinateMembers/CabinateMembers';
+import ExecutiveAbassador from './pages/Commitees/ExecutiveAmbassador/ExecutiveAbassador';
+import AdvisorCouncil from './pages/Commitees/AdvisoryCouncil/AdvisorCouncil';
+import PhotoGallary from './pages/Galllery/PhotoGallery/PhotoGallary';
+import Gallery from './pages/Galllery/Gallery';
+import VideoGallary from './pages/Galllery/VideoGallery/VideoGallary';
+import AnnirversaryModal from './component/AnnirversaryModal/AnnirversaryModal';
+import { useState } from 'react';
+import { useEffect } from 'react';
 function App() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 500);
+  }, []);
   return (
-    <div className="App">
+    <div className='App'>
       <ContextProvider>
         <BrowserRouter>
           <Header />
-          <div className="mainApp">
+          <div className='mainApp'>
+            <AnnirversaryModal
+              title='Anniversary Modal'
+              onClose={() => setShow(false)}
+              show={show}
+            ></AnnirversaryModal>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="contact" element={<Contact />} />
+              <Route path='/' element={<Home />} />
+              <Route path='about' element={<About />} />
+              <Route path='contact' element={<Contact />} />
               {/* committee  */}
-              <Route path="cabinateMembers" element={<CabinateMembers />} />
-              <Route path="exAmbassador" element={<ExecutiveAbassador />} />
-              <Route path="adCouncil" element={<AdvisorCouncil />} />
+              <Route path='cabinateMembers' element={<CabinateMembers />} />
+              <Route path='exAmbassador' element={<ExecutiveAbassador />} />
+              <Route path='adCouncil' element={<AdvisorCouncil />} />
               {/* Young Congress  */}
-              <Route path="YCAbout" element={<YCAbout />} />
-              <Route path="YCPrograms" element={<YCProgram />} />
-              <Route path="YCActivities" element={<YCAtivities />} />
-              <Route path="YCMembers" element={<YCMembers />} />
+              <Route path='YCAbout' element={<YCAbout />} />
+              <Route path='YCPrograms' element={<YCProgram />} />
+              <Route path='YCActivities' element={<YCAtivities />} />
+              <Route path='YCMembers' element={<YCMembers />} />
               {/* members  */}
-              <Route path="memberList" element={<MemberList />} />
-              <Route path="memberservices" element={<MemberServices />} />
-              <Route path="generalMembers" element={<GeneralMembers />} />
+              <Route path='memberList' element={<MemberList />} />
+              <Route path='memberservices' element={<MemberServices />} />
+              <Route path='generalMembers' element={<GeneralMembers />} />
               <Route
-                path="gmRegistration"
+                path='gmRegistration'
                 element={<GeneralMemberRegistration />}
               />
               <Route
-                path="emIndividualRegistration"
+                path='emIndividualRegistration'
                 element={<EMIndividualRegistration />}
               />
               <Route
-                path="emOrganizationRegistration"
+                path='emOrganizationRegistration'
                 element={<EMOrganization />}
               />
-              <Route path="membershipProcess" element={<MembershipProcess />} />
+              <Route path='membershipProcess' element={<MembershipProcess />} />
 
               {/* <Route path='memberList' element={}/>
             <Route path='memberservices' element={}/>
@@ -89,8 +103,8 @@ function App() {
             <Route path='membershipProcess' element={}/> */}
 
               {/* services  */}
-              <Route path="allservices" element={<AllServices />} />
-              <Route path="allservices/:id" element={<SingleServicePage />} />
+              <Route path='allservices' element={<AllServices />} />
+              <Route path='allservices/:id' element={<SingleServicePage />} />
 
               {/* <Route path='allservices' element={<Service />} /> */}
               {/* <Route path='consultant' element={ } />
@@ -103,16 +117,16 @@ function App() {
               {/* gallery  */}
 
               {/* gallery  */}
-              <Route path="gallery" element={<Gallery />} />
-              <Route path="gallery/photoGallery" element={<PhotoGallary />} />
+              <Route path='gallery' element={<Gallery />} />
+              <Route path='gallery/photoGallery' element={<PhotoGallary />} />
               {/* MINE  */}
-              <Route path="gallery/videoGallery" element={<VideoGallary />} />
+              <Route path='gallery/videoGallery' element={<VideoGallary />} />
               {/* END  */}
-              <Route path="events" element={<EventPage />} />
-              <Route path="events/:id" element={<EventDetailSingle />} />
+              <Route path='events' element={<EventPage />} />
+              <Route path='events/:id' element={<EventDetailSingle />} />
 
-              <Route path="blogs" element={<Blog />} />
-              <Route path="blogs/:id" element={<BlogDetailSingle />} />
+              <Route path='blogs' element={<Blog />} />
+              <Route path='blogs/:id' element={<BlogDetailSingle />} />
             </Routes>
           </div>
           <ScrollTop />
---
+++ src/component/AnnirversaryModal/AnnirversaryModal.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+import './AnnirversaryModal.css';
+import anniversaryImg from '../../assets/image/nrb anniversary.png';
+
+const AnnirversaryModal = (props) => {
+  console.log(props.show);
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeOnEscapeKeyDown);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={props.show}
+      unmountOnExit
+      timeout={{ enter: 10, exit: 300 }}
+    >
+      <div className='modal' onClick={props.onClose}>
+        <div className='modal-content' onClick={(e) => e.stopPropagation()}>
+          <div className='modal-header'>
+            <span
+              onClick={props.onClose}
+              className='closeButtonContainer flex align-items-end justify-content-end mr-2'
+            >
+              <i class='pi pi-times' style={{ fontSize: '1.5rem' }}></i>
+            </span>
+          </div>
+          <div className='modal-body'>
+            <div className='modalImageContainer'>
+              <img src={anniversaryImg} alt='3 Year Anniversary Celebration' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </CSSTransition>,
+    document.getElementById('root')
+  );
+};
+
+export default AnnirversaryModal;
