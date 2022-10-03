19619e66ba6bd3614b5a83139701c543871916ed
hasan75 <hasanjab14@gmail.com>
2022-10-03 20:31:30 +0600

    photo contest image added and fixes #71

--- src/pages/YoungCongress/YCAbout/YCAbout.js
+++ src/pages/YoungCongress/YCAbout/YCAbout.js
@@ -1,20 +1,21 @@
 import './YCAbout.css';
-import YCAboutSection from '../../../component/YoungCongress/YCAbout/YCAboutSection'
+import YCAboutSection from '../../../component/YoungCongress/YCAbout/YCAboutSection';
 import YCMisssion from '../../../component/YoungCongress/YCAbout/YCMisssion';
 import YCVision from '../../../component/YoungCongress/YCAbout/YCVision';
 import YCObjective from '../../../component/YoungCongress/YCAbout/YCObjective';
 import YCATeam from '../../../component/YoungCongress/YCAbout/YCATeam';
+import YCPhotoContestComponent from '../../../component/YoungCongress/YCAbout/YCPhotoContestComponent';
 function YCAbout() {
-    return (
-        <div className='yca-container p-container'>
-            <YCAboutSection />
-            <YCMisssion />
-            {/* <YCVision /> */}
-            <YCObjective />
-            <YCATeam />
-
-        </div>
-    )
+  return (
+    <div className='yca-container p-container'>
+      <YCAboutSection />
+      <YCMisssion />
+      {/* <YCVision /> */}
+      <YCObjective />
+      <YCATeam />
+      <YCPhotoContestComponent />
+    </div>
+  );
 }
 
-export default YCAbout
+export default YCAbout;
--- src/pages/YoungCongress/YCPhotoContest/YCPhotoContest.js
+++ src/pages/YoungCongress/YCPhotoContest/YCPhotoContest.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import photoContestBanner from '../../../assets/image/photoContests/photo Contest all photo/YC photo contest.jpg';
 import './YCPhotoContest.css';
-import { Image } from 'primereact/image';
-import img1 from '../../../assets/image/photoContests/photo Contest all photo/YC photo contest.jpg';
 
 import { photo_contest_pictures as photoContestImages } from '../../../data';
+import YCPhotoContestShow from '../../../component/YoungCongress/YCPhotoContest/YCPhotoContestShow';
 
 const YCPhotoContest = () => {
   return (
@@ -19,120 +18,121 @@ const YCPhotoContest = () => {
       </div>
       <div className='youngCongressPhotoContestPhotoContainer py-3'>
         {photoContestImages.map((photoInContest) => (
-          <section key={photoInContest.id}>
-            <div className='grid mb-3'>
-              <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                <Image
-                  imageClassName='imgPhotoGraphyContest'
-                  src={photoInContest?.imgInRow?.imgSeparate}
-                  alt='Photo Contest Image'
-                  width='100%'
-                  preview
-                  downloadable
-                />
-              </div>
-              <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                <div className='grid'>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInRow?.imgSeparateRow?.img1}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInRow?.imgSeparateRow?.img2}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInRow?.imgSeparateRow?.img3}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInRow?.imgSeparateRow?.img4}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='grid'>
-              <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                <div className='grid'>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img1}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img2}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img3}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                  <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                    <Image
-                      imageClassName='imgPhotoGraphyContest'
-                      src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img4}
-                      alt='Image'
-                      width='100%'
-                      preview
-                      downloadable
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
-                <Image
-                  imageClassName='imgPhotoGraphyContest'
-                  src={photoInContest?.imgInSecondRow?.imgSeparate}
-                  alt='Image'
-                  width='100%'
-                  preview
-                  downloadable
-                />
-              </div>
-            </div>
-          </section>
+          // <section key={photoInContest.id}>
+          //   <div className='grid mb-3'>
+          //     <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //       <Image
+          //         imageClassName='imgPhotoGraphyContest'
+          //         src={photoInContest?.imgInRow?.imgSeparate}
+          //         alt='Photo Contest Image'
+          //         width='100%'
+          //         preview
+          //         downloadable
+          //       />
+          //     </div>
+          //     <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //       <div className='grid'>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInRow?.imgSeparateRow?.img1}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInRow?.imgSeparateRow?.img2}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInRow?.imgSeparateRow?.img3}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInRow?.imgSeparateRow?.img4}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //       </div>
+          //     </div>
+          //   </div>
+          //   <div className='grid'>
+          //     <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //       <div className='grid'>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img1}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img2}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img3}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //         <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //           <Image
+          //             imageClassName='imgPhotoGraphyContest'
+          //             src={photoInContest?.imgInSecondRow?.imgSeparateRow?.img4}
+          //             alt='Image'
+          //             width='100%'
+          //             preview
+          //             downloadable
+          //           />
+          //         </div>
+          //       </div>
+          //     </div>
+          //     <div className='col-12 md:col-6 imgPhotoGraphyContestContainer'>
+          //       <Image
+          //         imageClassName='imgPhotoGraphyContest'
+          //         src={photoInContest?.imgInSecondRow?.imgSeparate}
+          //         alt='Image'
+          //         width='100%'
+          //         preview
+          //         downloadable
+          //       />
+          //     </div>
+          //   </div>
+          // </section>
+          <YCPhotoContestShow photoInContest={photoInContest} />
         ))}
       </div>
     </div>
