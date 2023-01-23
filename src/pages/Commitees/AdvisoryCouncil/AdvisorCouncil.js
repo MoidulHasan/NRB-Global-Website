3c7f49cb2d0a9c23de4c38c902c40921e00053ea
hasan75 <hasanjab14@gmail.com>
2023-01-23 15:07:27 +0600

    link added on become advisor button

--- src/pages/Commitees/AdvisoryCouncil/AdvisorCouncil.js
+++ src/pages/Commitees/AdvisoryCouncil/AdvisorCouncil.js
@@ -1,18 +1,21 @@
 import AdvisorCouncilCard from '../../../component/Commitees/AdvisorCouncil/AdvisorCouncilCard';
 import { Button } from 'primereact/button';
-import './AdvisorCouncil.css'
+import './AdvisorCouncil.css';
+import { Link } from 'react-router-dom';
 function AdvisorCouncil() {
-    return (
-        <div className='advisor-council-container p-container'>
-            <p className='advisor-council-container-para'>Advisor Council</p>
-            <AdvisorCouncilCard />
-            {/* <div className="advisorBtn">
+  return (
+    <div className='advisor-council-container p-container'>
+      <p className='advisor-council-container-para'>Advisor Council</p>
+      <AdvisorCouncilCard />
+      {/* <div className="advisorBtn">
                 <Button label="Become a Advisor Councilor" />
             </div> */}
-            <div className="advisor-memberBtn">
-                <Button label="Become a Advisor" />
-            </div>
-        </div>
-    )
+      <div className='advisor-memberBtn'>
+        <Link className='no-underline' to='/emIndividualRegistration'>
+          <Button label='Become an Advisor' />
+        </Link>
+      </div>
+    </div>
+  );
 }
-export default AdvisorCouncil
+export default AdvisorCouncil;
