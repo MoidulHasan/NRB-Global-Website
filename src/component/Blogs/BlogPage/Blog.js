1bbda648b706e462d8bb1c313c9906df9908978d
shamim392 <shaminur392@gmail.com>
2023-02-11 15:59:03 +0600

    link add

--- src/component/Blogs/BlogPage/Blog.js
+++ src/component/Blogs/BlogPage/Blog.js
@@ -10,37 +10,40 @@ const Blog = (props) => {
   // console.log("new-description :-", newDescription);
 
   return (
-    <div className="col-12 md:col-6 lg:col-4 xl:col-4 pb-5">
-      <div className="BlogImgContainer">
-        <img src={img} alt="" className="w-full" />
-      </div>
-      <div className="blogPageDetails">
-        <div className="flex justify-content-between pt-1">
-          <div className="flex align-items-center">
-            <i
-              className="pi pi-user font-bold mr-2 text-orange-600"
-              style={{ fontSize: "1.2em" }}
-            ></i>
-            <span className="font-semibold" style={{ fontSize: "1.2em" }}>
-              {auther}
-            </span>
-          </div>
-          <div className="flex align-items-center">
-            <i
-              className="pi pi-calendar-plus font-bold mr-2 text-orange-600"
-              style={{ fontSize: "1.2em" }}
-            ></i>
-            <span className="font-semibold" style={{ fontSize: "1.2em" }}>
-              {date}
-            </span>
-          </div>
+    <div className="col-12 md:col-6 lg:col-4 xl:col-4 pb-3">
+      <Link to={`/blogs/${id}`} className="no-underline">
+        <div className="BlogImgContainer">
+          <img src={img} alt="" className="w-full" />
         </div>
-        <span className="block text-xl font-bold mb-1 pb-3 pt-4 ">{title}</span>
-        <span className="block text-l text-gray-700 text-justify">
-          {newDescription}
-        </span>
-        <div className="mt-5">
-          {/* <div className="mt-4 flex align-items-center">
+        <div className="blogPageDetails">
+          <div className="flex justify-content-between pt-1">
+            <div className="flex align-items-center">
+              <i
+                className="pi pi-user font-bold mr-2 text-orange-600"
+                style={{ fontSize: "1.2em" }}
+              ></i>
+              <span className="font-semibold" style={{ fontSize: "1.2em" }}>
+                {auther}
+              </span>
+            </div>
+            <div className="flex align-items-center">
+              <i
+                className="pi pi-calendar-plus font-bold mr-2 text-orange-600"
+                style={{ fontSize: "1.2em" }}
+              ></i>
+              <span className="font-semibold" style={{ fontSize: "1.2em" }}>
+                {date}
+              </span>
+            </div>
+          </div>
+          <span className="block text-xl font-bold mb-1 pb-3 pt-4 ">
+            {title}
+          </span>
+          <span className="block text-l text-gray-700 text-justify">
+            {newDescription}
+          </span>
+          <div className="mt-5">
+            {/* <div className="mt-4 flex align-items-center">
             <i
               className="pi pi-heart mr-2 mt-1 font-bold text-orange-600"
               style={{ fontSize: "1.4em" }}
@@ -49,18 +52,19 @@ const Blog = (props) => {
               {like}
             </span>
           </div> */}
-          <div className="text-right">
-            <Link to={`/blogs/${id}`} className="no-underline">
-              <Button
-                label="Read More"
-                icon="pi pi-arrow-right"
-                className="p-button-sm pt-1 pb-1 p-button-secondary"
-                iconPos="right"
-              />
-            </Link>
+            <div className="text-right">
+              <Link to={`/blogs/${id}`} className="no-underline">
+                <Button
+                  label="Read More"
+                  icon="pi pi-arrow-right"
+                  className="p-button-sm pt-1 pb-1 p-button-secondary"
+                  iconPos="right"
+                />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
