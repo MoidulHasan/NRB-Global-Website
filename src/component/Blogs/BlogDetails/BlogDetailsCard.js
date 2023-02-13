87c889a72d3e7f5687aadf4aa9e340ad95ec5eb2
hasan75 <hasanjab14@gmail.com>
2023-02-14 00:10:08 +0600

    blog pages edited after intrigrated api

--- src/component/Blogs/BlogDetails/BlogDetailsCard.js
+++ src/component/Blogs/BlogDetails/BlogDetailsCard.js
@@ -1,87 +1,87 @@
-import React from "react";
-import { Divider } from "primereact/divider";
-import "./BlogDetailsCard.css";
+import React from 'react';
+import { Divider } from 'primereact/divider';
+import './BlogDetailsCard.css';
 
 const BlogDetailsCard = (singleBlog) => {
-  // console.log("single-Blog-Find", singleBlog.singleBlog.id);
+  console.log('single-Blog-Find', singleBlog?.singleBlog?.id);
 
   return (
-    <div className="p-container ">
-      <div className="p-4 my-4 blogDetailsPageContainer">
-        <div className="">
+    <div className='p-container '>
+      <div className='p-4 my-4 blogDetailsPageContainer'>
+        <div className=''>
           <img
-            src={singleBlog.singleBlog.img}
-            alt="blog_Image"
-            className="w-full  blogDetailsImg"
+            src={singleBlog?.singleBlog?.coverImage}
+            alt='blog_Image'
+            className='w-full  blogDetailsImg'
           />
         </div>
-        <div className="ml-2 md:ml-5lg:ml-5 xl:ml-5 mt-3">
-          <div className="flex ">
-            <div className="flex align-items-center mr-5">
+        <div className='ml-2 md:ml-5lg:ml-5 xl:ml-5 mt-3'>
+          <div className='flex '>
+            <div className='flex align-items-center mr-5'>
               <i
-                className="pi pi-calendar-plus font-bold mr-2 text-orange-500"
-                style={{ fontSize: "1.2em" }}
+                className='pi pi-calendar-plus font-bold mr-2 text-orange-500'
+                style={{ fontSize: '1.2em' }}
               ></i>
-              <span className="font-semibold">
-                {singleBlog.singleBlog.date}
+              <span className='font-semibold'>
+                {singleBlog?.singleBlog?.publishDate}
               </span>
             </div>
-            <div className="flex align-items-center mr-5">
+            <div className='flex align-items-center mr-5'>
               <i
-                className="pi pi-eye font-bold mr-2 text-orange-500"
-                style={{ fontSize: "1.2em" }}
+                className='pi pi-eye font-bold mr-2 text-orange-500'
+                style={{ fontSize: '1.2em' }}
               ></i>
-              <span className="font-semibold">
-                {singleBlog.singleBlog.view}
+              <span className='font-semibold'>
+                {singleBlog?.singleBlog?.view}
               </span>
             </div>
-            <div className="flex align-items-center mr-5">
+            <div className='flex align-items-center mr-5'>
               <i
-                className="pi pi-clock font-bold mr-2 text-orange-500"
-                style={{ fontSize: "1.2em" }}
+                className='pi pi-clock font-bold mr-2 text-orange-500'
+                style={{ fontSize: '1.2em' }}
               ></i>
-              <span className="font-semibold">
-                {singleBlog.singleBlog.read}
+              <span className='font-semibold'>
+                {singleBlog?.singleBlog?.readingTimeInMinute}
               </span>
             </div>
           </div>
         </div>
-        <div className="mt-6">
-          <p className="text-lg md:text-2xl lg:text-5xl font-bold text ml-2 md:ml-5 blogDetailsTitle">
-            {singleBlog.singleBlog.title}
+        <div className='mt-6'>
+          <p className='text-lg md:text-2xl lg:text-5xl font-bold text ml-2 md:ml-5 blogDetailsTitle'>
+            {singleBlog?.singleBlog?.title}
           </p>
-          <div className="flex align-items-center  ml-2 md:ml-5 mr-5">
-            <i className="pi pi-user font-bold mr-3 blogUserIcon"></i>
-            <span className="text-md md:text-3xl font-bold text-orange-500">
-              By {singleBlog.singleBlog.auther}
+          <div className='flex align-items-center  ml-2 md:ml-5 mr-5'>
+            <i className='pi pi-user font-bold mr-3 blogUserIcon'></i>
+            <span className='text-md md:text-3xl font-bold text-orange-500'>
+              By {singleBlog?.singleBlog?.authorName}
             </span>
           </div>
 
-          <Divider style={{ color: "#ff0000" }} />
-          <p className="px-2 md:px-5 lg:px-5 xl:px-5 text-xl md:text-3xl lg:text-3xl xl:text-3xl font-bold blogOverview">
+          <Divider style={{ color: '#ff0000' }} />
+          <p className='px-2 md:px-5 lg:px-5 xl:px-5 text-xl md:text-3xl lg:text-3xl xl:text-3xl font-bold blogOverview'>
             Blog Overview:
           </p>
-          <p className="px-2 md:px-5 lg:px-5 xl:px-5 text-justify blogDetailsContent">
-            {singleBlog.singleBlog.description}
+          <p className='px-2 md:px-5 lg:px-5 xl:px-5 text-justify blogDetailsContent'>
+            {singleBlog?.singleBlog?.content}
           </p>
         </div>
-        <Divider style={{ color: "#ff0000" }} />
+        <Divider style={{ color: '#ff0000' }} />
 
-        <div className="text-left md:text-right">
-          <span className="text-xl font-bold text-orange-500 mr-2">
-            Share:{" "}
+        <div className='text-left md:text-right'>
+          <span className='text-xl font-bold text-orange-500 mr-2'>
+            Share:{' '}
           </span>
-          <a href="https://www.facebook.com/NRBGlobal20">
-            <i className="pi pi-facebook mr-3  text-blue-900 socialMediaIocn"></i>
+          <a href='https://www.facebook.com/NRBGlobal20'>
+            <i className='pi pi-facebook mr-3  text-blue-900 socialMediaIocn'></i>
           </a>
-          <a href="https://www.linkedin.com/company/young-cogress-nrb-global/">
-            <i className="pi pi-linkedin mr-3  text-orange-600 socialMediaIocn"></i>
+          <a href='https://www.linkedin.com/company/young-cogress-nrb-global/'>
+            <i className='pi pi-linkedin mr-3  text-orange-600 socialMediaIocn'></i>
           </a>
-          <a href="#">
-            <i className="pi pi-whatsapp mr-3 lg text-green-600 socialMediaIocn"></i>
+          <a href='#'>
+            <i className='pi pi-whatsapp mr-3 lg text-green-600 socialMediaIocn'></i>
           </a>
-          <a href="#">
-            <i className="pi pi-twitter mr-2 md:mr-4  text-cyan-600 socialMediaIocn"></i>
+          <a href='#'>
+            <i className='pi pi-twitter mr-2 md:mr-4  text-cyan-600 socialMediaIocn'></i>
           </a>
         </div>
       </div>
--- src/component/Blogs/BlogPage/Blog.js
+++ src/component/Blogs/BlogPage/Blog.js
@@ -1,48 +1,49 @@
-import React from "react";
-import { Button } from "primereact/button";
-import "./BlogPage.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Button } from 'primereact/button';
+import './BlogPage.css';
+import { Link } from 'react-router-dom';
 
 const Blog = (props) => {
-  const { id, title, auther, date, description, img } = props.blogsinfo;
+  const { id, title, authorName, publishDate, content, coverImage, page } =
+    props.blogsinfo;
   // const newDescription = description;
-  const newDescription = description.slice(0, 201);
+  const newDescription = content.slice(0, 201);
   // console.log("new-description :-", newDescription);
 
   return (
-    <div className="col-12 md:col-6 lg:col-4 xl:col-4 pb-3">
-      <Link to={`/blogs/${id}`} className="no-underline">
-        <div className="BlogImgContainer">
-          <img src={img} alt="" className="w-full" />
+    <div className='col-12 md:col-6 lg:col-4 xl:col-4 pb-3'>
+      <Link to={`/blogs/${id}`} className='no-underline'>
+        <div className='BlogImgContainer'>
+          <img src={coverImage} alt='' className='w-full' />
         </div>
-        <div className="blogPageDetails">
-          <div className="flex justify-content-between pt-1">
-            <div className="flex align-items-center">
+        <div className='blogPageDetails'>
+          <div className='flex justify-content-between pt-1'>
+            <div className='flex align-items-center'>
               <i
-                className="pi pi-user font-bold mr-2 text-orange-600"
-                style={{ fontSize: "1.2em" }}
+                className='pi pi-user font-bold mr-2 text-orange-600'
+                style={{ fontSize: '1.2em' }}
               ></i>
-              <span className="font-semibold" style={{ fontSize: "1.2em" }}>
-                {auther}
+              <span className='font-semibold' style={{ fontSize: '1.2em' }}>
+                {authorName}
               </span>
             </div>
-            <div className="flex align-items-center">
+            <div className='flex align-items-center'>
               <i
-                className="pi pi-calendar-plus font-bold mr-2 text-orange-600"
-                style={{ fontSize: "1.2em" }}
+                className='pi pi-calendar-plus font-bold mr-2 text-orange-600'
+                style={{ fontSize: '1.2em' }}
               ></i>
-              <span className="font-semibold" style={{ fontSize: "1.2em" }}>
-                {date}
+              <span className='font-semibold' style={{ fontSize: '1.2em' }}>
+                {publishDate}
               </span>
             </div>
           </div>
-          <span className="block text-xl font-bold mb-1 pb-3 pt-4 ">
+          <span className='block text-xl font-bold mb-1 pb-3 pt-4 '>
             {title}
           </span>
-          <span className="block text-l text-gray-700 text-justify">
+          <span className='block text-l text-gray-700 text-justify'>
             {newDescription}
           </span>
-          <div className="mt-5">
+          <div className='mt-5'>
             {/* <div className="mt-4 flex align-items-center">
             <i
               className="pi pi-heart mr-2 mt-1 font-bold text-orange-600"
@@ -52,13 +53,13 @@ const Blog = (props) => {
               {like}
             </span>
           </div> */}
-            <div className="text-right">
-              <Link to={`/blogs/${id}`} className="no-underline">
+            <div className='text-right'>
+              <Link to={`/blogs/${id}`} className='no-underline'>
                 <Button
-                  label="Read More"
-                  icon="pi pi-arrow-right"
-                  className="p-button-sm pt-1 pb-1 p-button-secondary"
-                  iconPos="right"
+                  label='Read More'
+                  icon='pi pi-arrow-right'
+                  className='p-button-sm pt-1 pb-1 p-button-secondary'
+                  iconPos='right'
                 />
               </Link>
             </div>
