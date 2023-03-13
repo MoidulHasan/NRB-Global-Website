5f274b3012349b67e2fe8b4d8038e44ca08a530f
Najim Uddin <najim7304@gmail.com>
2023-03-13 20:30:47 +0600

    video gallery done

--- src/component/Galllery/VideoGallery/VideoGallaryGrid.js
+++ src/component/Galllery/VideoGallery/VideoGallaryGrid.js
@@ -1,42 +1,50 @@
 import { useEffect, useState } from "react";
 import "./VideoGallaryGrid.css";
-import { Paginator } from 'primereact/paginator';
+import { slice } from "lodash";
+import { Button } from "primereact/button";
 function VideoGallaryGrid() {
     const url = process.env.REACT_APP_BACKEND_URL;
-    const [gallary, setGallery] = useState([]);
-    const [page, setPage] = useState(1);
-    useEffect(() => {
-        fetch(`${url}/gallery?page=${page}`)
+    const [currentItem, setCurrentItem] = useState([]);
+    const [isCompleted, setIsCompleted] = useState(false);
+    const [index, setIndex] = useState(5);
+    const initialImg = slice(currentItem, 0, index);
+    const videoData = () => {
+        fetch(`${url}/gallery`)
             .then((res) => res.json())
-            .then((data) => setGallery(data?.data?.results));
-    }, [page]);
-    const [basicFirst, setBasicFirst] = useState(0);
-    const [basicRows, setBasicRows] = useState(10);
-    const onPageChange = (event) => {
-        setPage(event.page + 1);
-        setBasicFirst(event.first);
-        setBasicRows(event.rows);
-    }
-    const galleryVideo = gallary?.filter((video) => video.type === "Video");
+            .then((data) => setCurrentItem(data?.data?.results));
+    };
+    const galleryVideo = initialImg?.filter((video) => video.type === "Video");
+    const loadMore = () => {
+        setIndex(index + 5);
+        console.log(index);
+        if (index >= currentItem.length) {
+            setIsCompleted(true);
+        } else {
+            setIsCompleted(false);
+        }
+    };
+    useEffect(() => {
+        videoData();
+    }, []);
     return (
         <div className="video-Gallary p-container">
             <div className="video-gallary-row">
                 <h3>Video Gallary</h3>
                 <div className="video-gallary-column grid">
-                    {galleryVideo.map((item, i) => (
+                    {galleryVideo.slice(0, 4).map((item, i) => (
                         <div className="col-12 md:col-4" key={i}>
                             <iframe width="640" height="360" src={item.url} allowFullScreen />
                         </div>
                     ))}
                 </div>
+                <div className="flex align-items-center justify-content-center my-4">
+                    {isCompleted ? (
+                        <Button onClick={loadMore} label="That's It" />
+                    ) : (
+                        <Button onClick={loadMore} label="Load More" severity="secondary" />
+                    )}
+                </div>
             </div>
-            <Paginator
-                totalRecords={gallary?.totalResults}
-                onPageChange={onPageChange}
-                first={basicFirst}
-                rows={basicRows}
-                rowsPerPageOptions={gallary?.limit}
-            />
         </div>
     );
 }
