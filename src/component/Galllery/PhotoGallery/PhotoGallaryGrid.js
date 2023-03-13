d803bf50984060a686af86ff6ef61e53bf51672a
Najim Uddin <najim7304@gmail.com>
2023-03-13 23:10:40 +0600

    gallery Video done

--- src/component/Galllery/PhotoGallery/PhotoGallaryGrid.js
+++ src/component/Galllery/PhotoGallery/PhotoGallaryGrid.js
@@ -1,58 +1,41 @@
 import "./PhotoGallaryGrid.css";
 import { Image } from "primereact/image";
 import { useState, useEffect } from "react";
-import { slice } from "lodash";
 import { Button } from "primereact/button";
 function PhotoGallaryGrid() {
     const url = process.env.REACT_APP_BACKEND_URL;
     const [currentItem, setCurrentItem] = useState([]);
-    const [isCompleted, setIsCompleted] = useState(false);
-    const [index, setIndex] = useState(5);
-    const initialImg = slice(currentItem, 0, index)
+    const [page, setPage] = useState(1);
+    const [totalpage, setTotalPage] = useState();
     const imgData = () => {
-        fetch(`${url}/gallery`)
+        if (totalpage === page) return;
+        fetch(`${url}/gallery?type=Photo&limit=20&page=${page}`)
             .then((res) => res.json())
-            .then((data) => setCurrentItem(data?.data?.results));
-    }
-    const galleryImg = initialImg?.filter((img) => img.type === "Photo");
+            .then((data) => {
+                setCurrentItem([...currentItem, ...data?.data?.results]);
+                setTotalPage(data?.data?.totalPages);
+            });
+    };
     const loadMore = () => {
-        setIndex(index + 5);
-        console.log(index);
-        if (index >= currentItem.length) {
-            setIsCompleted(true);
-        } else {
-            setIsCompleted(false);
-        }
+        setPage(page + 1);
     };
     useEffect(() => {
         imgData();
-    }, []);
+    }, [page]);
     return (
         <div className="photo-Gallary p-container">
             <div className="photo-gallary-row">
                 <h3>Photo Gallary</h3>
                 <div className="phtoto-gallary-column grid">
-                    {galleryImg.slice(0, 4).map((item, i) => (
+                    {currentItem.map((item, i) => (
                         <div className="col-12 md:col-3" key={i}>
                             <Image src={item.url} alt="Image" width="250" preview />
                         </div>
                     ))}
                 </div>
                 <div className="flex align-items-center justify-content-center my-4">
-                    {isCompleted ? (
-                        <Button
-                            onClick={loadMore}
-                            label="That's It"
-                        />
-
-
-                    ) : (
-                        <Button
-                            onClick={loadMore}
-                            label="Load More"
-                            severity="secondary"
-
-                        />
+                    {totalpage >= page && (
+                        <Button onClick={loadMore} label="Load More" severity="secondary" />
                     )}
                 </div>
             </div>
--- src/component/Galllery/VideoGallery/VideoGallaryGrid.js
+++ src/component/Galllery/VideoGallery/VideoGallaryGrid.js
@@ -1,46 +1,39 @@
 import { useEffect, useState } from "react";
 import "./VideoGallaryGrid.css";
-import { slice } from "lodash";
 import { Button } from "primereact/button";
 function VideoGallaryGrid() {
     const url = process.env.REACT_APP_BACKEND_URL;
     const [currentItem, setCurrentItem] = useState([]);
-    const [isCompleted, setIsCompleted] = useState(false);
-    const [index, setIndex] = useState(5);
-    const initialImg = slice(currentItem, 0, index);
+    const [page, setPage] = useState(1);
+    const [totalpage, setTotalPage] = useState();
     const videoData = () => {
-        fetch(`${url}/gallery`)
+        if (totalpage === page) return;
+        fetch(`${url}/gallery?type=Video&limit=20&page=${page}`)
             .then((res) => res.json())
-            .then((data) => setCurrentItem(data?.data?.results));
+            .then((data) => {
+                setCurrentItem([...currentItem, ...data?.data?.results]);
+                setTotalPage(data?.data?.totalPages);
+            });
     };
-    const galleryVideo = initialImg?.filter((video) => video.type === "Video");
     const loadMore = () => {
-        setIndex(index + 5);
-        console.log(index);
-        if (index >= currentItem.length) {
-            setIsCompleted(true);
-        } else {
-            setIsCompleted(false);
-        }
+        setPage(page + 1);
     };
     useEffect(() => {
         videoData();
-    }, []);
+    }, [page]);
     return (
         <div className="video-Gallary p-container">
             <div className="video-gallary-row">
                 <h3>Video Gallary</h3>
                 <div className="video-gallary-column grid">
-                    {galleryVideo.slice(0, 4).map((item, i) => (
+                    {currentItem.map((item, i) => (
                         <div className="col-12 md:col-4" key={i}>
                             <iframe width="640" height="360" src={item.url} allowFullScreen />
                         </div>
                     ))}
                 </div>
                 <div className="flex align-items-center justify-content-center my-4">
-                    {isCompleted ? (
-                        <Button onClick={loadMore} label="That's It" />
-                    ) : (
+                    {totalpage >= page && (
                         <Button onClick={loadMore} label="Load More" severity="secondary" />
                     )}
                 </div>
