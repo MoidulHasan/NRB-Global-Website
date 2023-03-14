260038bc10cccd874808cc82314b3ef603ef8587
hasan75 <hasanjab14@gmail.com>
2023-03-14 21:08:31 +0600

    fixes executive member form

--- src/component/Blogs/BlogPage/BlogPage.js
+++ src/component/Blogs/BlogPage/BlogPage.js
@@ -8,23 +8,24 @@ import { Paginator } from 'primereact/paginator';
 
 const BlogPage = () => {
   const [blogInfo, setBlogInfo] = useState([]);
+  const url = process.env.REACT_APP_BACKEND_URL;
   useEffect(() => {
     fetch(`${url}/blogs`)
       .then((res) => res.json())
       .then((data) => setBlogInfo(data?.data?.results));
   }, []);
   return (
-    <div className="p-container mb-4">
-      <div className="pt-5 pb-3 px-4">
-        <div className="">
-          <img src={blog} alt="" className="w-full blogPageImg" />
+    <div className='p-container mb-4'>
+      <div className='pt-5 pb-3 px-4'>
+        <div className=''>
+          <img src={blog} alt='' className='w-full blogPageImg' />
         </div>
-        <div className="">
-          <h1 className="text-center text-6xl font-bold text-cyan-900 blogPageTitle">
+        <div className=''>
+          <h1 className='text-center text-6xl font-bold text-cyan-900 blogPageTitle'>
             Our All Blogs
           </h1>
         </div>
-        <div className="grid">
+        <div className='grid'>
           {blogInfo.map((blogsinfo) => (
             <Blog key={blogsinfo._id} blogsinfo={blogsinfo}></Blog>
           ))}
